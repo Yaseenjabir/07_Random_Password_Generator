a61cf72f3fdbf5c8a183b716969acad0509b1437
Yaseenjabir <yaseenkhan1235476@gmail.com>
2024-02-06 14:51:30 +0000

    version 1.0.0

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-const input = document.querySelector("input");
+
 const btn = document.querySelector('button');
+const main = document.querySelector('.main');
+const input = document.querySelector('input')
+const copy = document.querySelector(".input-icon i");
 
-btn.addEventListener('click',()=>{
 
-  const length = 12
+btn.addEventListener('click',()=>
+{
+  const length = 12;
+  const charset = 'qwertyuiopasdfghjklzxcvbnm1234567890!@#$%^&*()_+=-?"'
   let password = "";
-  const charSet = 'qwertyuiopasdfghjklzxcvbnm1234567890~`!@#$%^&*()-_=+:;/.<>'
-  for(i=0; i<length; i++){
-    const randomIndex = Math.floor(Math.random() * charSet.length);
-    password += charSet[randomIndex];
+  
+  for(i=0; i < length; i++){
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset[randomIndex];
   }
+  
   input.value = password;
-})
+});
 window.addEventListener('beforeunload',()=>{
   input.value = "";
+})
+
+copy.addEventListener('click', ()=>{
+  const passwordValue = input.value;
+  input.select();
+  document.execCommand('copy');
+  window.getSelection().removeAllRanges();
 })
